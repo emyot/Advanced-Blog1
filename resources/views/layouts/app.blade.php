b36a0f352c5efb7caad0fd449ae2011a5a6d445f
Rocky <rockyugsod@gmail.com>
2017-10-21 11:18:10 +0800

    Navigation Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,11 +19,58 @@
         <div class="navbar-menu">
           <div class="navbar-start">
             <a class="navbar-item" href="{{ 'home' }}">
-              <img src="{{ asset('images/devmarketer-logo.png') }}" alt="DevMarketer Logo">
+              <img src="{{ asset('images/onepiece-logo.png') }}" alt="DevMarketer Logo">
             </a>
             <a href="#" class="navbar-item is-tab is-hidden-mobile">Learn</a>
             <a href="#" class="navbar-item is-tab is-hidden-mobile">Discuss</a>
             <a href="#" class="navbar-item is-tab is-hidden-mobile">Share</a>
+          </div>
+
+          <div class="navbar-end">
+            @if(Auth::guest())
+              <a href="#" class="navbar-item is-tab">Login</a>
+              <a href="#" class="navbar-item is-tab">Sign Up</a>
+            @else
+              <div class="dropdown is-right is-hoverable">
+                <div class="dropdown-trigger">
+                  <a class="navbar-item is-tab" aria-haspopup="true" aria-controls="dropdown-menu">
+                    <span>Hello Slacker</span>
+                    <span class="icon is-small">
+                      <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                </div>
+                <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                  <div class="dropdown-content">
+                    <a href="#" class="dropdown-item">
+                      <span class="icon is-small">
+                        <i class="fa fa-user-circle" aria-hidden="true"></i>
+                      </span>
+                      Profile
+                    </a>
+                    <a href="#" class="dropdown-item">
+                      <span class="icon is-small">
+                        <i class="fa fa-bell" aria-hidden="true"></i>
+                      </span>
+                      Notifications
+                    </a>
+                    <a href="#" class="dropdown-item">
+                      <span class="icon is-small">
+                        <i class="fa fa-cog" aria-hidden="true"></i>
+                      </span>
+                      Settings
+                    </a>
+                    <hr class="dropdown-divider">
+                    <a href="#" class="dropdown-item">
+                      <span class="icon is-small">
+                        <i class="fa fa-sign-out" aria-hidden="true"></i>
+                      </span>
+                      Logout
+                    </a>
+                  </div>
+                </div>
+              </div>
+            @endif
           </div>
         </div>
       </nav>
